f189add23958843710e6eca4335a051cd8793a83
Dang Pham <pham_d1@denison.edu>
2022-03-24 10:33:08 -0400

    retrain model + push data folder
    still need to add more features to the program

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 import os
+from tensorflow import keras
 from keras.preprocessing import image
 import matplotlib.pyplot as plt 
 import numpy as np
@@ -8,6 +9,7 @@ from keras.models import Sequential
 from keras.layers import Dropout,Conv2D,Flatten,Dense, MaxPooling2D, BatchNormalization
 from keras.models import load_model
 
+print(os.curdir)
 
 def generator(dir, gen=image.ImageDataGenerator(rescale=1./255), shuffle=True,batch_size=1,target_size=(24,24),class_mode='categorical' ):
 
@@ -19,7 +21,7 @@ train_batch= generator('data/train',shuffle=True, batch_size=BS,target_size=TS)
 valid_batch= generator('data/valid',shuffle=True, batch_size=BS,target_size=TS)
 SPE= len(train_batch.classes)//BS
 VS = len(valid_batch.classes)//BS
-print(SPE,VS)
+#print(SPE,VS)
 
 
 # img,labels= next(train_batch)
@@ -47,11 +49,33 @@ model = Sequential([
 #one more dropout for convergence' sake :) 
     Dropout(0.5),
 #output a softmax to squash the matrix into output probabilities
-    Dense(2, activation='softmax')
+    Dense(4, activation='softmax')
 ])
+'''
+for i in range(1):
+    print("Loop #: " + str(i))
+    model = load_model('models/cnncat2.h5')
+    opt = keras.optimizers.Adam(learning_rate=0.01)
 
-model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
+    model.compile(optimizer=opt,loss='categorical_crossentropy',metrics=['accuracy'])
+    history1 = model.fit(train_batch, validation_data=valid_batch,epochs=100,steps_per_epoch=SPE ,validation_steps=VS)
 
-model.fit_generator(train_batch, validation_data=valid_batch,epochs=15,steps_per_epoch=SPE ,validation_steps=VS)
+    model.save('models/cnnCat2.h5', overwrite=True)
+'''
 
-model.save('models/cnnCat2.h5', overwrite=True)
+opt = keras.optimizers.Adam(learning_rate=0.01)
+model.compile(optimizer=opt,loss='categorical_crossentropy',metrics=['accuracy'])
+history1 = model.fit(train_batch, validation_data=valid_batch,epochs=20,steps_per_epoch=SPE ,validation_steps=VS)
+
+#print(history1.history.keys())
+
+loss_train = history1.history['loss']
+loss_val = history1.history['accuracy']
+epochs = range(1,21)
+plt.plot(epochs, loss_train, 'g', label='Loss')
+plt.plot(epochs, loss_val, 'b', label='Accuracy')
+plt.title('Model''s Accuracy and Loss')
+plt.xlabel('Epochs')
+plt.ylabel('Percentage')
+plt.legend()
+plt.show()
